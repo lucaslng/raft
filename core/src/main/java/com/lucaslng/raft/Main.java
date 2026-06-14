8e4a7d83e1f5fb7ce34cf1717272ec4c5e377d05
Lucas Leung <saculgnuel1@gmail.com>
2026-06-14 03:01:41 -0400

    intro comment

--- core/src/main/java/com/lucaslng/raft/Main.java
+++ core/src/main/java/com/lucaslng/raft/Main.java
@@ -1,3 +1,9 @@
+// Lucas Leung
+// Sunday, June 15th, 2026
+// Survive the harsh seas on your little raft!
+// Collect trash, build structures, and manage your hunger and thirst. Build a sail to control your raft. Find the rumoured safe haven far up north, but be careful of the Sharks!
+// Built using libGDX
+
 package com.lucaslng.raft;
 
 import com.badlogic.gdx.Game;
